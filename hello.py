d5ed54a581cee38deb9deedcb4fdd02f561ee689
Madalin Clipici <clipicimadalin10@yahoo.com>
2022-04-07 20:28:17 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from mypackage.my_first_module import my_func as my_module_func, my_var as my_module_var
 from mypackage.my_second_module import *
 
-if __name__ == '__main__':
+def main():
     print(my_module_var)
     my_module_func()
 
